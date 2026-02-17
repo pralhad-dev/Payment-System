07944e6e3671a97a9c0f882493a1829361590a6e
pralhad gadlegave <pralhadgadlegave55@gmail.com>
2026-02-17 21:27:35 +0530

    Transaction Apis : 1. fetch transaction details by txnId. 2. fetch all transactions list.

--- src/main/java/com/paymentflow/payment/controller/TransactionController.java
+++ src/main/java/com/paymentflow/payment/controller/TransactionController.java
@@ -2,6 +2,7 @@ package com.paymentflow.payment.controller;
 
 import com.paymentflow.payment.dto.CreateUserRequest;
 import com.paymentflow.payment.dto.GlobalApiResponse;
+import com.paymentflow.payment.dto.TransactionResponse;
 import com.paymentflow.payment.dto.TransferRequest;
 import com.paymentflow.payment.exception.DataNotFoundException;
 import com.paymentflow.payment.service.TransactionService;
@@ -11,10 +12,9 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/transactions")
@@ -26,12 +26,23 @@ public class TransactionController {
         this.transactionService = transactionService;
     }
 
-
-
     @Operation(summary = "transfer money", description = "user can transfer money to another user")
     @PostMapping(value = "/transfer",produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<GlobalApiResponse<TransferRequest>> transfterMoney(@Valid @RequestBody TransferRequest request) throws DataNotFoundException {
         return new ResponseEntity<>(transactionService.transferMoney(request),HttpStatus.OK);
     }
+
+    @Operation(summary = "transaction details",description = "Fetch all transaction details by transaction id")
+    @GetMapping(value = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<GlobalApiResponse<TransactionResponse>> fetchTransactionById(@PathVariable Long id) throws DataNotFoundException {
+        return new ResponseEntity<>(transactionService.fetchTransactionsById(id),HttpStatus.OK);
+    }
+
+    @Operation(summary = "All transaction List",description = "Return all Transaction list")
+    @GetMapping(value = "/list",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<GlobalApiResponse<List<TransactionResponse>>> getAllTrxnList(){
+        return new ResponseEntity<>(transactionService.getAllTxn(),HttpStatus.OK);
+    }
+
 
 }
--- src/main/java/com/paymentflow/payment/service/TransactionService.java
+++ src/main/java/com/paymentflow/payment/service/TransactionService.java
@@ -1,9 +1,16 @@
 package com.paymentflow.payment.service;
 
 import com.paymentflow.payment.dto.GlobalApiResponse;
+import com.paymentflow.payment.dto.TransactionResponse;
 import com.paymentflow.payment.dto.TransferRequest;
 import com.paymentflow.payment.exception.DataNotFoundException;
 
+import java.util.List;
+
 public interface TransactionService {
      GlobalApiResponse<TransferRequest> transferMoney(TransferRequest request) throws DataNotFoundException;
+
+     GlobalApiResponse<TransactionResponse> fetchTransactionsById(Long id) throws DataNotFoundException;
+
+     GlobalApiResponse<List<TransactionResponse>> getAllTxn();
 }
